588e4ce9d229b4dd6cf748edf0372ecac4bf1fc1
DiegoDev <diegorego516@gmail.com>
2026-07-02 14:20:51 -0300

    Add files via upload

---
+++ test1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void *malloc(size_t size );
+void free(void *ptr);
+void *calloc(size_t nmemb , size_t size );
+void *realloc(void *ptr , size_t size );
+
+char MPI_CHAR;
+unsigned char MPI_BYTE;
+short MPI_SHORT;
+int MPI_INT;
+long MPI_LONG;
+float MPI_FLOAT;
+double MPI_DOUBLE;
+unsigned char MPI_UNSIGNED_CHAR;
+unsigned short MPI_UNSIGNED_SHORT;
+unsigned int MPI_UNSIGNED;
+unsigned long MPI_UNSIGNED_LONG;
+long double MPI_LONG_DOUBLE;
+long long MPI_LONG_LONG_INT;
+
+int main () {
+return 0;
+printf("%s\nThis is a test... it is not the final product");
+
+
+return 0;
+printf("%s\nThis is a test... it is not the final product");
+}
